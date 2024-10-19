e28b2df6100a52c025f1f7c96bea57d2ee41f4a7
Jose Tejada <jose.tejada.ext@tlscontact.com>
2024-10-19 10:30:44 +0100

    Removing firebase config info

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     define: {
-        "process.env": {
-            REACT_APP_FIREBASE_API_KEY: "AIzaSyAOb7w0FKqi0A2FDlXPssl_Yg_qe1xJeQA",
-            REACT_APP_FIREBASE_AUTH_DOMAIN: "finalapp-9ec11.firebaseapp.com",
-            REACT_APP_FIREBASE_PROJECT_ID: "finalapp-9ec11",
-            REACT_APP_FIREBASE_STORAGE_BUCKET: "finalapp-9ec11.appspot.com",
-            REACT_APP_FIREBASE_MESSAGING_SENDER_ID: "171387902481",
-            REACT_APP_FIREBASE_APP_ID: "finalapp-9ec11",
-        },
+        "process.env": {},
     },
 });
