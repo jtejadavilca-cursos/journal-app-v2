e9e25beb400ec29f4564212db6c835c17a85b385
Jose Tejada <jtejadavilca@gmail.com>
2024-10-20 23:11:00 +0100

    Using environment variables from .env file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore/lite";
 
+const vars = import.meta.env;
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: vars.VITE_FIREBASE_API_KEY,
+    authDomain: vars.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: vars.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: vars.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: vars.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: vars.VITE_FIREBASE_APP_ID,
 };
 
 export const FirebaseApp = initializeApp(firebaseConfig);
